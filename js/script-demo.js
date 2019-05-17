e4d259065ef108a1ff8dff7b20916838c525252c
Huseyn <you@example.com>
2019-05-17 17:26:56 +0200

    Add a demo page, enable video capturing and file upload

---
+++ js/script-demo.js
@@ -0,0 +1,98 @@
+$(function () {
+  "use strict";
+
+  /*-----------------------------------
+   * FIXED  MENU - HEADER
+   *-----------------------------------*/
+  function menuscroll() {
+    var $navmenu = $('.nav-menu');
+    if ($(window).scrollTop() > 50) {
+      $navmenu.addClass('is-scrolling');
+    } else {
+      $navmenu.removeClass("is-scrolling");
+    }
+  }
+  menuscroll();
+  $(window).on('scroll', function () {
+    menuscroll();
+  });
+  /*-----------------------------------
+   * NAVBAR CLOSE ON CLICK
+   *-----------------------------------*/
+
+  $('.navbar-nav > li:not(.dropdown) > a').on('click', function () {
+    $('.navbar-collapse').collapse('hide');
+  });
+  /* 
+   * NAVBAR TOGGLE BG
+   *-----------------*/
+  var siteNav = $('#navbar');
+  siteNav.on('show.bs.collapse', function (e) {
+    $(this).parents('.nav-menu').addClass('menu-is-open');
+  })
+  siteNav.on('hide.bs.collapse', function (e) {
+    $(this).parents('.nav-menu').removeClass('menu-is-open');
+  })
+
+  /*-----------------------------------
+   * ONE PAGE SCROLLING
+   *-----------------------------------*/
+  // Select all links with hashes
+  // Select all links with hashes
+  $('a[href*="#"]')
+    // Remove links that don't actually link to anything
+    .not('[href="#"]')
+    .not('[href="#0"]')
+    .click(function (event) {
+      // On-page links
+      if (
+        location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '')
+        &&
+        location.hostname == this.hostname
+      ) {
+        // Figure out element to scroll to
+        var target = $(this.hash);
+        target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+        // Does a scroll target exist?
+        if (target.length) {
+          // Only prevent default if animation is actually gonna happen
+          event.preventDefault();
+          $('html, body').animate({
+            scrollTop: target.offset().top
+          }, 1000, function () {
+            // Callback after animation
+            // Must change focus!
+            var $target = $(target);
+            $target.focus();
+            if ($target.is(":focus")) { // Checking if the target was focused
+              return false;
+            } else {
+              $target.attr('tabindex', '-1'); // Adding tabindex for elements not focusable
+              $target.focus(); // Set focus again
+            };
+          });
+        }
+      }
+    });
+
+
+  /*-----------------------------------
+  * INITIALIZE THE WEBCAM
+  *-----------------------------------*/
+  Webcam.attach('#my_camera');
+  var take_snapshot = document.getElementById('take-snapshot').addEventListener('click', take_snapshot)
+
+  function take_snapshot() {
+    Webcam.snap(function (data_uri) {
+      document.getElementById('my_result').innerHTML = '<img src="' + data_uri + '"/>';
+    });
+  }
+
+  $("#openCameraModal").animatedModal({ animatedIn: 'lightSpeedIn', animatedOut: 'bounceOutDown'});
+
+  FilePond.parse(document.body);
+
+  $('#uploadPhoto').on('FilePond:addfile', function (e) {
+    console.log('file added event', e);
+  });
+});
